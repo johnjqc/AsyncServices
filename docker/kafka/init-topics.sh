09928d0872bcfb86da78e71cb5617cf8d9a8dd20
John Quiroga <johnquirogac@gmail.com>
2026-06-04 11:56:01 -0500

    configure kafka topic

---
+++ docker/kafka/init-topics.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+echo "Waiting for Kafka..."
+sleep 15
+
+echo "Creating topics..."
+
+kafka-topics --bootstrap-server kafka:9092 \
+  --create --if-not-exists \
+  --topic client.created \
+  --partitions 1 \
+  --replication-factor 1
+
+
+echo "Topics ready"
